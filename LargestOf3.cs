b2ae2a2d99cd90a9d08b7eb9581430e977d7643d
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 13:40:09 +0530

    Add files via upload
    largest of three added prog

---
+++ LargestOf3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestofThreeNumbers
+{
+    internal class LargestOf3
+    {
+        public static void largest3()
+        {
+            //int num1 = 10;
+            //int num2 = 85;
+            //int num3 = 25;
+            Console.WriteLine("Enter a number1 to get largest of three numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number2 to get largest of three numbers");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number3 to get largest of three numbers");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1>num2 && num2 >num3)
+            {
+                Console.WriteLine("num1 is Largest Number");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("num2 is Largest Number");
+            }
+            else
+            {
+                Console.WriteLine("num3 is Largest Number");
+            }
+        }
+        }
+}
