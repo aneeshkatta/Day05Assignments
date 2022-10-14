671116821f26b1574b939fa9d233d86d7bb55d70
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 13:36:42 +0530

    Add files via upload
    quotient remainder added prog

---
+++ CalcQUOTIENT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeQuotientRem
+{
+    internal class CalcQUOTIENT
+    {
+        public static void quotient()
+        {
+            Console.WriteLine("Enter a dividend");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a divisor");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Quotient is"+num / num1);
+            Console.WriteLine("Remainder is"+num % num1);
+        }
+    }
+}
