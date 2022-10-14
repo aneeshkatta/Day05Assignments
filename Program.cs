8039e21ebee1c29701944c9b9801303d810b58c7
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 13:37:37 +0530

    Add files via upload
    swap number added prog

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumber
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            SwapNum.Swapping();
+        }
+    }
+}
---
+++ SwapNum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumber
+{
+    public class SwapNum
+    {
+        public static void Swapping()
+        {
+            int a = 20;
+            int b = 30;
+            Console.WriteLine("Value of a and b before swapping");
+            Console.WriteLine("a="  + a);
+            Console.WriteLine("b=" + b);
+            int tempswap = a;
+            a = b;
+            b = tempswap;
+           Console.WriteLine("Value of a and b after swapping");
+            Console.WriteLine("a=" +  a);
+            Console.WriteLine("b=" +  b);
+         
+        }
+    }
+
+}
