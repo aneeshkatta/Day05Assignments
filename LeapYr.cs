f0a2cbeed2891a57810ebe835d51b3ec7b1898a6
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 13:31:52 +0530

    Add files via upload
    leap yr program added

---
+++ LeapYr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    internal class LeapYr
+            {
+        public static void YearCheck()
+        {
+            Console.WriteLine("Enter a year");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            if (num%4==0)
+            {
+
+                Console.WriteLine("Year is a Leap year " + num );
+            }
+            else
+            {
+                Console.WriteLine("Year is not a Leap year " + num);
+            }
+        }
+    }
+}
