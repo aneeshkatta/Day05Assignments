2139184cfbab22038702223fea5a9ade31e45bd5
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 13:38:37 +0530

    Add files via upload
    even odd added

---
+++ Checkevenodd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    internal class Checkevenodd
+    {
+       public static void evenodd()
+        {
+            Console.WriteLine("Enter a number to check its even or odd");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("no. is a even");
+                }
+                else
+                {
+                    Console.WriteLine("no. is a Odd");
+                }
+        }
+
+    }
+    
+}
