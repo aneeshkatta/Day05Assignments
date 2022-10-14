6eaff5a04bdb1912ee8ae623062d531eba745af2
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 13:39:20 +0530

    Add files via upload
    vowel consonent added prog

---
+++ VowelConsonentcheck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelsConsonents
+{
+    internal class VowelConsonentcheck
+    {
+        public static void Check()
+        {
+            Console.Write("Input an Alphabet (A-Z or a-z) : ");
+            Char Word = Convert.ToChar(Console.ReadLine().ToLower());
+
+            switch (Word)
+            {
+                case 'a':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                default:
+                    Console.WriteLine("The Alphabet is not a vowel");
+                    break;
+            }
+        }
+        }
+}
