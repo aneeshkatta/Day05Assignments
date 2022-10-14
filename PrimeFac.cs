2b59152bee12701027a3d9ec9e2205b6aa66dd49
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 13:34:58 +0530

    Add files via upload
    prime factoriczation added

---
+++ PrimeFac.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactorization
+{
+    public class PrimeFac
+    {
+        public static void Prime()
+        {
+            Console.WriteLine("Enter a number to get prime factors with Logic -> Traverse till i*i <= N instead of i <= N");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for (int b = 1; b*b <= a; b++)
+            {
+                if (a % b == 0)
+                {
+                    Console.WriteLine(b + " is a factor of " + a);
+                }
+            }
+        }
+    }
+}
